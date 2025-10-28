782617d7daa2be9fa73aaf8fbe42fac9a9c68f45
João Victor Santos <jvictor.pereira.santos@gmail.com>
2025-10-28 11:08:16 -0300

    removed exposed api urls

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -6,7 +6,7 @@ import {
   ReactNode,
 } from "react";
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:3000";
+const API_BASE_URL = import.meta.env.VITE_API_URL
 
 interface User {
   email: string;
